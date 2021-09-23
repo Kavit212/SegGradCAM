85e1315fab87d44170e31b750f0e4b16a700da09
Kavit212 <kavitha.shaga.devan@gmail.com>
2021-09-23 10:05:17 -0700

    test

--- seggradcam/visualize_sgc.py
+++ seggradcam/visualize_sgc.py
@@ -85,7 +85,7 @@ class SegGradCAMplot(SegGradCAM):
         # plt.axis('off')
         plt.imshow(self.ximg, vmin=0, vmax=1, cmap=self.cmap_orig)
         # class contour
-        X, Y = self.roi.meshgrid()
+        #X, Y = self.roi.meshgrid()
 
         if pixel:
             # i, j = self.roi.i, self.roi.j
@@ -136,92 +136,5 @@ class SegGradCAMplot(SegGradCAM):
         start_save_name = 'pixel' + str(i) + '_' + str(j) + '_cl'
         self.explainBase(title1, title1bias, start_save_name, pixel=True)
 
-    def baseGtPrediction(self, title2, start_save_name_tetra, pixel=False):
-
-        if self.next_dict and self.image_id:
-            self.gt = self.next_dict[1][self.image_id]
-        if type(self.gt) == 'NoneType':  # .all()==None:
-            # raise error
-            print("Provide the groundtruth mask")
-
-        classes_cmap, scale_fig, fonts, scatter_size = self.defaultScales()
-        plt.figure(figsize=(30 * scale_fig, 15 * scale_fig))
-        plt.subplot(221)
-        plt.imshow(self.ximg, vmin=0, vmax=1, cmap=self.cmap_orig)
-        plt.title('Input image', fontsize=fonts)
-        X, Y = self.roi.meshgrid()
-        # biased texture contour
-        if self.next_dict and self.image_id:
-            biasroi = BiasRoI(self.next_dict, self.image_id)
-            #plt.contour(X, Y, biasroi.biased_mask, colors='magenta')  # 'black') #'purple')
-            # print(biasroi.biased_mask.any()==0, biasroi.biased_mask.all()==0)
-            if biasroi.biased_mask.any() != 0:
-                plt.title('Input image & biased texture', fontsize=fonts)
-        if pixel:
-            i, j = self.roi.i, self.roi.j
-            plt.scatter(j, i, color='red', s=scatter_size)
-        plt.tick_params(labelsize=fonts)
-
-        plt.subplot(222)
-        plt.axis('off')
-        plt.imshow(self.ximg, vmin=0, vmax=1, cmap=self.cmap_orig)
-        if pixel:
-            plt.scatter(j, i, color='white', s=scatter_size)
-        else:
-            # class contour
-            #plt.contour(X, Y, self.roi.roi, colors='pink')
-
-        plt.title(title2, fontsize=fonts)
-        plt.imshow(self.cam, cmap='jet',  # vmin=0,vmax=1,
-                   alpha=0.6)
-        jet = plt.colorbar(fraction=0.046, pad=0.04, ticks=[0, 0.2, 0.4, 0.6, 0.8, 1])
-        jet.set_label(label="Importance", size=fonts)
-        jet.ax.set_yticklabels([0, 0.2, 0.4, 0.6, 0.8, 1], size=fonts)
-
-        plt.subplot(223)
-        plt.axis('off')
-        plt.imshow(np.argmax(self.gt, axis=-1), vmin=0, vmax=self.n_classes - 1, cmap=classes_cmap)
-        plt.title('Groundtruth mask', fontsize=fonts)
-        if pixel:
-            plt.scatter(j, i, color='white', s=scatter_size)
-
-        plt.subplot(224)
-        plt.axis('off')
-        predictions = self.model.predict(np.expand_dims(self.image, 0))[0]
-        plt.imshow(np.argmax(predictions, axis=-1), vmin=0, vmax=self.n_classes - 1, cmap=classes_cmap)
-        # plt.colorbar(fraction=0.046, pad=0.04,cmap=cm)
-        cbar = plt.colorbar(cmap=classes_cmap, fraction=0.046, pad=0.04, ticks=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-        cbar.set_label(label="Classes", size=fonts)
-        # categories =['Void','Flat','Const-\nruction','Object','Nature','Sky','Human','Vehicle']
-        cbar.ax.tick_params(labelsize=fonts)
-        # cbar.ax.set_yticklabels(categories, size=fonts)
-
-        if pixel:
-            plt.scatter(j, i, color='white', s=scatter_size)
-
-        plt.title('Predicted mask', fontsize=fonts)
-        plt.tight_layout(pad=0.1, w_pad=15, h_pad=5.0)
-
-        plt.savefig(os.path.join(self.outfolder,
-                                 start_save_name_tetra + str(self.cls) + '_to_act' + self.prop_to_layer.split('_')[1] +'_'+
-                                 self.timestr + ".png"))
-
-    def classGtPrediction(self):
-        """Plot 4 images: original, ground truth, predicted mask, seg-grad-cam explanations for a selected class channel"""
-        title2 = 'Seg-Grad-CAM for class %d' % (self.cls)
-        start_save_name_tetra = 'tetra_class'
-        self.baseGtPrediction(title2, start_save_name_tetra)
-
-    def roiGtPrediction(self):
-        """Plot 4 images: original, ground truth, predicted mask, seg-grad-cam explanations for a region of interest"""
-        title2 = 'Seg-Grad-CAM for RoI(pink) of class %d' % (self.cls)
-        start_save_name_tetra = 'tetra_roi'
-        self.baseGtPrediction(title2, start_save_name_tetra)
-
-    def pixelGtPrediction(self):
-        """Plot 4 images: original, ground truth, predicted mask, seg-grad-cam explanations for a selected single pixel"""
-        i, j = self.roi.i, self.roi.j
-        title2 = 'Seg-Grad-CAM for pixel [%d,%d]. Class %d' % (i, j, self.cls)
-        start_save_name_tetra = 'tetra_pixel' + str(i) + '_' + str(j) + '_cl'
-        self.baseGtPrediction(title2, start_save_name_tetra, pixel=True)
-
+  
+ 
